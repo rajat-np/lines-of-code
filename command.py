dce532c9aa3aa86b85d0d57ae798bea1c74aacbd
Rajat Soni <rajat.tcp@gmail.com>
2021-01-13 17:56:08 +0530

    Update arguement name

--- command.py
+++ command.py
@@ -6,14 +6,14 @@ from lines_of_code.main import get_lines_of_code
 if __name__ == '__main__':        
     arg_parser = argparse.ArgumentParser(prog='lines_of_code', description='Count lines of code in a source file')
 
-    arg_parser.add_argument('Path',
+    arg_parser.add_argument('path',
                             metavar='path',
                             type=str,
                             help='path to the source file')
 
     args = arg_parser.parse_args()
 
-    input_path = args.Path
+    input_path = args.path
 
     result = get_lines_of_code(input_path)
 
